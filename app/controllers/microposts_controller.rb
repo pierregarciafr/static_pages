70a49bc98488e27dc8e7e06cffb0ff89dd92cc31
Pierre Garcia <pg.pierre.garcia@free.fr>
2020-05-03 22:21:38 +0200

    create feed destroy and tests

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
 before_action :logged_in_user, only: [:create, :destroy]
+before_action :correct_user, only: [:destroy]
 
   def create
     @micropost = current_user.microposts.build(micropost_params)
@@ -7,28 +8,26 @@ before_action :logged_in_user, only: [:create, :destroy]
       flash[:info] = "Micropost created!"
       redirect_to root_url
     else
+      @feed_items = current_user.feed.paginate(page: params[:page])
       render 'static_pages/home'
     end
   end
 
   def destroy
-    @micropost = Micropost.find(params[:id])
-    @user = @micropost.user
-    if @user == current_user
-      @micropost.delete
-      flash[:info] = "Post deleted!"
-      redirect_to user_path(@user)
-    else
-      flash[:danger] = "You can't delete this post. "
-      flash[:danger] += 'Who do you think you are ?'
-      redirect_to user_path(@user)
-    end
+      @micropost.destroy
+      flash[:success] = "Post deleted!"
+      redirect_to request.referrer || root_url
   end
 
   private
 
   def micropost_params
     params.require(:micropost).permit(:content)
+  end
+
+  def correct_user
+    @micropost = current_user.microposts.find(params[:id])
+    redirect_to root_url if @micropost.nil?
   end
 
 end
