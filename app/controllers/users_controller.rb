e9a85aa8218dd52b15436214dce4543fc4a69eb8
Pierre Garcia <pg.pierre.garcia@free.fr>
2020-05-06 16:01:36 +0200

    create stats and view for followers and following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
-  before_action :correct_user,   only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update, :following, :followers]
   before_action :admin_user, only: :destroy
 
   def index
@@ -46,6 +46,15 @@ class UsersController < ApplicationController
     User.find(params[:id]).destroy
     flash[:success] = "User deleted"
     redirect_to users_url
+  end
+
+  def following
+
+    @users = @user.following
+  end
+
+  def followers
+    @users = @user.followers
   end
 
   private
--- test/models/relationship_test.rb
+++ test/models/relationship_test.rb
@@ -20,4 +20,24 @@ class RelationshipTest < ActiveSupport::TestCase
     @relationship.followed_id = nil
     assert_not @relationship.valid?
   end
+
+  test 'should follow and unfollow a user' do # methods follow and unfollow and .following?
+    michael = users(:michael)
+    archer = users(:archer)
+    assert_not michael.following?(archer)
+    michael.follow(archer)
+    assert michael.following?(archer)
+    michael.unfollow(archer)
+    assert_not michael.following?(archer)
+  end
+
+  test 'should be followed by a user' do # methods follow and unfollow and .following?
+    michael = users(:michael)
+    archer = users(:archer)
+    assert_not archer.followed_by?(michael)
+    michael.follow(archer)
+    assert archer.followed_by?(michael)
+    michael.unfollow(archer)
+    assert_not archer.followed_by?(michael)
+  end
 end
